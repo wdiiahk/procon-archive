6a665f8df23147ce783f4d639dc09c379a2c2fcf
wdiiahk <hikida0842@gmail.com>
2020-10-03 01:46:42 +0900

    [AC] abc085 abc085_c

---
+++ atcoder.jp/abc085/abc085_c/Main.cpp
@@ -0,0 +1,117 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename... Args>
+std::string to_str_by(const std::string &fmt, Args... args)
+{
+  size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(len + 1);
+  std::snprintf(&buf[0], len + 1, fmt.c_str(), args...);
+  return std::string(&buf[0], &buf[0] + len);
+}
+
+ll get_target(ll s, ll N, ll z)
+{
+  if ((s - z) % 5 != 0)
+  {
+    return -1;
+  }
+  ll target = (s - z) / 5;
+  if (3 * N < target)
+  {
+    return -1;
+  }
+  return target;
+}
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll N, S;
+  cin >> N >> S;
+  // 10000 * x + 5000 * y + 1000 * z = S
+  //  5 * (2 * x + y) = (S/1000) - z
+  // target = ((S / 10000) - z) / 5
+  // (2 * x + y) = target
+  //  x + y + z = N
+  // (2 * x + N - x - z) = target
+  // (x + N - z) = target
+  // x = target - N + z
+  ll ret = 0;
+  ll s = S / 1000;
+  REP(z, N + 1)
+  {
+    ll target = get_target(s, N, z);
+    if (target < 0)
+    {
+      continue;
+    }
+    ll x = target - N + z;
+    if (x < 0 || N < x)
+    {
+      continue;
+    }
+    ll y = N - x - z;
+    if (y < 0 || N < y)
+    {
+      continue;
+    }
+
+    cout << x << " " << y << " " << z << endl;
+    return 0;
+  }
+  cout << "-1 -1 -1" << endl;
+  return 0;
+}
