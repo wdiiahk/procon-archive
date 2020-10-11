62d2f4255f77552a726a448f7697e872fed32a56
wdiiahk <hikida0842@gmail.com>
2020-10-11 23:15:27 +0900

    [AC] arc105 arc105_b

---
+++ atcoder.jp/arc105/arc105_b/Main.cpp
@@ -0,0 +1,119 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename... Args>
+std::string to_str_by(const std::string &fmt, Args... args)
+{
+  size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(len + 1);
+  std::snprintf(&buf[0], len + 1, fmt.c_str(), args...);
+  return std::string(&buf[0], &buf[0] + len);
+}
+
+struct Args
+{
+  int N;     // 1 <= N <= 10^5
+  set<ll> a; // 1 <= a <= 10^9
+  Args()
+  {
+    cin >> N;
+    REP(i, N)
+    {
+      ll tmp;
+      cin >> tmp;
+      a.insert(tmp);
+    }
+  }
+};
+
+class Solver
+{
+private:
+  /* data */
+  Args &args;
+
+public:
+  Solver(Args &args) : args(args) {}
+  ll solve()
+  {
+    while (true)
+    {
+      auto it0 = args.a.begin();
+      auto it1 = args.a.rbegin();
+      ll tmp = *it1 - *it0;
+      if (tmp == 0)
+      {
+        return *it0;
+      }
+      args.a.erase((++it1).base());
+      args.a.insert(tmp);
+    }
+    return INF;
+  }
+
+  void output(ll ans)
+  {
+    cout << ans << endl;
+  }
+};
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  Args args;
+  Solver s(args);
+  ll ans = s.solve();
+  s.output(ans);
+}
