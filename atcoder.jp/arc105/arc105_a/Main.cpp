383dfe4a1fa8e03d8ee2cc0ac764fc0e9abfb93a
wdiiahk <hikida0842@gmail.com>
2020-10-11 22:42:14 +0900

    [AC] arc105 arc105_a

---
+++ atcoder.jp/arc105/arc105_a/Main.cpp
@@ -0,0 +1,102 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename... Args>
+std::string to_str_by(const std::string &fmt, Args... args)
+{
+  size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(len + 1);
+  std::snprintf(&buf[0], len + 1, fmt.c_str(), args...);
+  return std::string(&buf[0], &buf[0] + len);
+}
+
+bool dfs(vector<ll> &a, ll half, int i, int s)
+{
+  if (i == 4)
+  {
+    return (s == half);
+  }
+  if (dfs(a, half, i + 1, s))
+  {
+    return true;
+  }
+  if (dfs(a, half, i + 1, s + a.at(i)))
+  {
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll A, B, C, D;
+  cin >> A >> B >> C >> D;
+  ll total = A + B + C + D;
+  if (total % 2 != 0)
+  {
+    cout << "No" << endl;
+    return 0;
+  }
+  ll half = total / 2;
+  vector<ll> tmp({A, B, C, D});
+  if (dfs(tmp, half, 0, 0))
+  {
+    cout << "Yes" << endl;
+    return 0;
+  }
+  cout << "No" << endl;
+  return 0;
+}
