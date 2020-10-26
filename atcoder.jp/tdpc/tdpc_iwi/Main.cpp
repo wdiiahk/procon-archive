1c32d39c7bdebf7333701f9d15ed2e38808f665d
wdiiahk <hikida0842@gmail.com>
2020-10-26 14:49:13 +0900

    [AC] tdpc tdpc_iwi

---
+++ atcoder.jp/tdpc/tdpc_iwi/Main.cpp
@@ -0,0 +1,196 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename... Args>
+std::string to_str_by(const std::string &fmt, Args... args)
+{
+  size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(len + 1);
+  std::snprintf(&buf[0], len + 1, fmt.c_str(), args...);
+  return std::string(&buf[0], &buf[0] + len);
+}
+
+template <class T>
+int popcount(T &a)
+{
+  int c = 0;
+  REP(i, 8 * (int)sizeof(a))
+  {
+    if ((a >> i) & 1)
+      c++;
+  }
+  return c;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(P &p) { cout << p.first << " " << p.second << endl; }
+
+/**
+ * s は i と w からなる文字列である。
+ * すぬけ君は、この中から連続する三文字が "iwi" となっている部分を取り除く操作を繰り返すことができる。
+ * (iwi を取り除くと、その左側と右側にあった文字列が連結され、長さが三文字短い新しい文字列ができる。) 
+ * 操作を行うことのできる回数の最大値を求めよ。 
+ */
+struct Args
+{
+  string s;
+  ll N;
+  Args()
+  {
+    cin >> s;
+    N = s.length();
+  }
+};
+
+class Solver
+{
+private:
+  /* data */
+  Args &args;
+  ll ans;
+  arr<ll> dp;
+
+public:
+  Solver(Args &args) : args(args), ans(0)
+  {
+    dp.resize(args.N + 1, vll(args.N + 1, -1));
+  }
+
+  ll dfs(ll l, ll r)
+  {
+    if (0 <= dp.at(l).at(r))
+    {
+      return dp.at(l).at(r);
+    }
+    if (l == r)
+    {
+      return dp.at(l).at(r) = 0;
+    }
+    //
+    ll res(0);
+    // 1. 全部取り除ける時
+    if (args.s.at(l) == 'i' && args.s.at(r - 1) == 'i')
+    {
+      FOR(i, l + 1, r - 1)
+      {
+        if (args.s.at(i) != 'w')
+        {
+          continue;
+        }
+        if (dfs(l + 1, i) == i - (l + 1) && dfs(i + 1, r - 1) == r - 1 - (i + 1))
+        {
+          chmax(res, r - l);
+        }
+      }
+    }
+    // 2. そうでない時
+    FOR(i, l + 1, r)
+    {
+      chmax(res, dfs(l, i) + dfs(i, r));
+    }
+
+    // cout << l << " " << r << " = " << res << endl;
+    return dp.at(l).at(r) = res;
+  }
+
+  void solve()
+  {
+    ans = dfs(0, args.N) / 3;
+  }
+  void output()
+  {
+    pr(ans);
+  }
+};
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  Args args;
+  Solver s(args);
+  s.solve();
+  s.output();
+}
