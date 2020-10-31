3ce0e0829315b2abab0e144671e6952e18f919a0
wdiiahk <hikida0842@gmail.com>
2020-10-31 21:14:58 +0900

    [AC] arc107 arc107_a

---
+++ atcoder.jp/arc107/arc107_a/Main.cpp
@@ -0,0 +1,164 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 998244353     //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename... Args>
+std::string to_str_by(const std::string &fmt, Args... args)
+{
+  size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(len + 1);
+  std::snprintf(&buf[0], len + 1, fmt.c_str(), args...);
+  return std::string(&buf[0], &buf[0] + len);
+}
+
+template <class T>
+int popcount(T &a)
+{
+  int c = 0;
+  REP(i, 8 * (int)sizeof(a))
+  {
+    if ((a >> i) & 1)
+      c++;
+  }
+  return c;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(P &p) { cout << p.first << " " << p.second << endl; }
+
+/**
+ * 3 つの正整数 A,B,C が与えられます。
+ * ∑Aa=1∑Bb=1∑Cc=1abc を 998244353
+ * で割った余りを求めてください。
+ */
+struct Args
+{
+  ll A, B, C;
+  Args()
+  {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    A = a % MOD;
+    B = b % MOD;
+    C = c % MOD;
+  }
+};
+
+class Solver
+{
+private:
+  /* data */
+  Args &args;
+  ll ans;
+
+public:
+  Solver(Args &args) : args(args), ans(1)
+  {
+  }
+  void solve()
+  {
+    ll tmp = args.A * (args.A + 1) / 2;
+    ans *= (tmp % MOD);
+    tmp = args.B * (args.B + 1) / 2;
+    ans *= (tmp % MOD);
+    ans %= MOD;
+    tmp = args.C * (args.C + 1) / 2;
+    ans *= (tmp % MOD);
+    ans %= MOD;
+  }
+  void output()
+  {
+    pr(ans);
+  }
+};
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  Args args;
+  Solver s(args);
+  s.solve();
+  s.output();
+}
