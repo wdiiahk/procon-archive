bcebb19f0b56330fccbadc0a276fa3f777a54a1e
wdiiahk <hikida0842@gmail.com>
2020-11-01 21:07:20 +0900

    [AC] abc181 abc181_b

---
+++ atcoder.jp/abc181/abc181_b/Main.cpp
@@ -0,0 +1,167 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename... Args>
+std::string to_str_by(const std::string &fmt, Args... args)
+{
+  size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args...);
+  std::vector<char> buf(len + 1);
+  std::snprintf(&buf[0], len + 1, fmt.c_str(), args...);
+  return std::string(&buf[0], &buf[0] + len);
+}
+
+template <class T>
+int popcount(T &a)
+{
+  int c = 0;
+  REP(i, 8 * (int)sizeof(a))
+  {
+    if ((a >> i) & 1)
+      c++;
+  }
+  return c;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(P &p) { cout << p.first << " " << p.second << endl; }
+
+/**
+ * 何も書かれていない黒板があります。 高橋くんは N 回の操作を行い、黒板に整数を書きます
+ * i 回目の操作では、 Ai 以上 Bi 以下の整数すべてを 1 個ずつ、合計 Bi−Ai+1 個の整数を書きます。
+ * N回の操作を終えたときの、黒板に書かれた整数の合計を求めてください。
+ */
+struct Args
+{
+  ll N;
+  vll A, B;
+  Args()
+  {
+    cin >> N;
+    A.resize(N);
+    B.resize(N);
+    REP(i, N)
+    {
+      cin >> A.at(i) >> B.at(i);
+    }
+  }
+};
+
+class Solver
+{
+private:
+  /* data */
+  Args &args;
+  ll ans;
+
+public:
+  Solver(Args &args) : args(args), ans(0)
+  {
+  }
+  void solve()
+  {
+    ans = 0;
+    REP(i, args.N)
+    {
+      ll A = args.A.at(i);
+      ll B = args.B.at(i);
+      ll sum = (B - A + 1) * (A + B) / 2;
+      ans += sum;
+    }
+  }
+  void output()
+  {
+    pr(ans);
+  }
+};
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  Args args;
+  Solver s(args);
+  s.solve();
+  s.output();
+}
