4b32774b4f8f4f10a97acf9177a25c0cffbeafd1
wdiiahk <hikida0842@gmail.com>
2020-11-18 15:38:20 +0900

    [AC] abc084 abc084_d

---
+++ atcoder.jp/abc084/abc084_d/Main.cpp
@@ -0,0 +1,136 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(const vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(const arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(const P &p) { cout << p.first << " " << p.second << endl; }
+
+vbool f(100001, false);
+vll c(100002, 0);
+ll N, L, R;
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  FOR(i, 2, 100001)
+  {
+    if (f.at(i))
+    {
+      continue;
+    }
+    for (ll j = 2 * i; j < 100001; j += i)
+    {
+      f.at(j) = true;
+    }
+  }
+  for (ll i = 3; i < 100001; i += 2)
+  {
+    if (!f.at(i) && !f.at((i + 1) / 2))
+    {
+      c.at(i)++;
+    }
+  }
+
+  FOR(i, 3, 100001)
+  {
+    c.at(i) += c.at(i - 1);
+  }
+
+  ll Q;
+  cin >> Q;
+  vll ans(Q);
+  REP(i, Q)
+  {
+    ll l, r;
+    cin >> l >> r;
+    ans.at(i) = c.at(r) - c.at(l - 1);
+  }
+  //
+  FORA(a, ans)
+  {
+    pr(a);
+  }
+  return 0;
+}
