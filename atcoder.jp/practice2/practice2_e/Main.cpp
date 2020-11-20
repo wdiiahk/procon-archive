67e85dac76b626092c11e4c9dd9e568bd71fa4b4
wdiiahk <hikida0842@gmail.com>
2020-11-20 23:33:56 +0900

    [AC] practice2 practice2_e

---
+++ atcoder.jp/practice2/practice2_e/Main.cpp
@@ -0,0 +1,139 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(const vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(const arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(const P &p) { cout << p.first << " " << p.second << endl; }
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll N, K;
+  cin >> N >> K;
+
+  // generate (s -> row -> column -> t) graph
+  // i-th row correspond to vertex i
+  // i-th col correspond to vertex n + i
+  mcf_graph<ll, ll> g(2 * N + 2);
+  ll s = 2 * N;
+  ll t = 2 * N + 1;
+  g.add_edge(s, t, N * K, INF);
+  REP(i, N)
+  {
+    g.add_edge(s, i, K, 0);
+    g.add_edge(N + i, t, K, 0);
+  }
+  REP(i, N)
+  {
+    REP(j, N)
+    {
+      ll a;
+      cin >> a;
+      g.add_edge(i, N + j, 1, INF - a);
+    }
+  }
+  //
+  auto result = g.flow(s, t, N * K);
+  ll ans = N * K * INF - result.second;
+  //
+  vector<string> grid(N, string(N, '.'));
+  FORA(e, g.edges())
+  {
+    if (e.from == s || e.to == t || e.flow == 0)
+    {
+      continue;
+    }
+    grid.at(e.from).at(e.to - N) = 'X';
+  }
+  //
+  pr(ans);
+  FORA(line, grid)
+  {
+    pr(line);
+  }
+  return 0;
+}
