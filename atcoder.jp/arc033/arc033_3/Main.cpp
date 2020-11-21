fc0760583570d76b9f45ef94331bbe48c7c4521f
wdiiahk <hikida0842@gmail.com>
2020-11-21 17:48:12 +0900

    [AC] arc033 arc033_3

---
+++ atcoder.jp/arc033/arc033_3/Main.cpp
@@ -0,0 +1,152 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(const vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(const arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(const P &p) { cout << p.first << " " << p.second << endl; }
+
+#define XMAX 220000
+
+ll op(ll a, ll b)
+{
+  return a + b;
+}
+
+ll e()
+{
+  return 0;
+}
+
+int main()
+{
+  //入力の高速化用のコード
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll Q;
+  cin >> Q;
+  segtree<ll, op, e> seg(XMAX);
+  REP(i, Q)
+  {
+    int t;
+    cin >> t;
+    if (t == 1)
+    {
+      ll x;
+      cin >> x;
+      seg.set(x, 1);
+    }
+    else if (t == 2)
+    {
+      ll x;
+      cin >> x;
+      ll res = seg.max_right(0, [&](ll s) {
+        return s < x;
+      });
+      pr(res);
+      seg.set(res, 0);
+
+      /*
+      ll left = 0;
+      ll right = XMAX;
+      while (1 < right - left)
+      {
+        ll mid = (left + right) / 2;
+        if (x <= seg.prod(0, mid))
+        {
+          right = mid;
+        }
+        else
+        {
+          left = mid;
+        }
+      }
+      pr(left);
+      seg.set(left, 0);
+      */
+    }
+  }
+}
