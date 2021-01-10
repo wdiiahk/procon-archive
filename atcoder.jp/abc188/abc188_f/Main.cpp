1ea0d2416d2d0c10def40d1134ca50da327909f3
wdiiahk <hikida0842@gmail.com>
2021-01-10 23:10:41 +0900

    [AC] abc188 abc188_f

---
+++ atcoder.jp/abc188/abc188_f/Main.cpp
@@ -0,0 +1,163 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(const vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(const arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(const P &p) { cout << p.first << " " << p.second << endl; }
+
+struct Constants
+{
+  ll X, Y;
+  Constants()
+  {
+    cin >> X >> Y;
+  }
+} C;
+
+struct Args
+{
+  Args()
+  {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+  }
+} args;
+
+struct Solver
+{
+  ll ans;
+  map<ll, ll> dp;
+  Solver() : ans(0)
+  {
+  }
+  ll dfs(ll y) {
+    if (dp.count(y) != 0) {
+      return dp[y];
+    }
+    if (y == C.X) {
+      return 0;
+    }
+
+    dp[y] = abs(C.X - y);
+    if (y == 1) {
+      //
+    }
+    else if (y % 2 == 0) {
+      chmin(dp[y], dfs(y / 2) + 1);
+    }
+    else {
+      chmin(dp[y], 2 + dfs((y + 1)/2));
+      chmin(dp[y], 2 + dfs((y - 1)/2));
+    }
+    return dp[y];
+  }
+
+  //
+  void solve()
+  {
+    if (C.X == C.Y) {
+      return;
+    }
+    if (C.Y < C.X) {
+      ans = C.X - C.Y;
+      return;
+    }
+    ans = dfs(C.Y);
+  }
+  //
+  void output()
+  {
+    pr(ans);
+  }
+} s;
+
+int main()
+{
+  s.solve();
+  s.output();
+  return 0;
+}
