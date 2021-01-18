26de317fc13fa8d385f52e313da2281f2906c30b
wdiiahk <hikida0842@gmail.com>
2021-01-19 05:03:30 +0900

    [AC] abc084 abc084_c

---
+++ atcoder.jp/abc084/abc084_c/Main.cpp
@@ -0,0 +1,162 @@
+//デバッグ用オプション：-fsanitize=undefined,address
+
+//コンパイラ最適化
+#pragma GCC optimize("Ofast")
+
+//インクルードなど
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vbool = vector<bool>;
+template <class T>
+using arr = vector<vector<T>>;
+
+//マクロ
+//forループ
+//引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
+//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+//FORAは範囲for文(使いにくかったら消す)
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i < ll(b); i++)
+#define FORD(i, a, b) for (ll i = b - 1; i >= ll(a); i--)
+#define FORA(i, I) for (const auto &i : I)
+//xにはvectorなどのコンテナ
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+//定数
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007    //10^9+7:合同式の法
+#define MAXR 100000       //10^5:配列の最大のrange
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+void pl(T x) { cout << x << " "; }
+
+template <class T>
+void pr(T x) { cout << x << endl; }
+
+template <class T>
+void prvec(const vector<T> &a)
+{
+  REP(i, a.size() - 1)
+  {
+    cout << a[i] << " ";
+  }
+  pr(a[a.size() - 1]);
+}
+
+template <class T>
+void prarr(const arr<T> &a)
+{
+  REP(i, a.size())
+  if (a[i].empty())
+    pr("");
+  else
+    prvec(a[i]);
+}
+
+using P = pair<ll, ll>;
+void prp(const P &p) { cout << p.first << " " << p.second << endl; }
+
+struct Constants
+{
+  ll N;
+  Constants()
+  {
+    cin >> N;
+  }
+} C;
+
+struct Args
+{
+  vll c, s, f;
+  Args() : c(C.N - 1), s(C.N - 1), f(C.N - 1)
+  {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    REP(i, C.N - 1)
+    {
+      cin >> c.at(i) >> s.at(i) >> f.at(i);
+    }
+  }
+} args;
+
+struct Solver
+{
+  vll ans;
+  vector<map<ll, ll>> dp;
+  Solver() : ans(C.N, 0), dp(C.N)
+  {
+  }
+  ll dfs(ll x, ll s) {
+    if (C.N - 1 <= x) {
+      return s;
+    }
+    if (s < args.s.at(x)) {
+      s = args.s.at(x);
+    }
+    else {
+      s = ((s + args.f.at(x) - 1)  / args.f.at(x)) * args.f.at(x);
+    }
+    if (0 < dp.at(x).count(s)) {
+      return dp.at(x).at(s);
+    }
+    ll res = dfs(x+1, s + args.c.at(x));
+    return dp.at(x)[s] = res;
+  }
+
+  //
+  void solve()
+  {
+    REPD(i, C.N - 1) {
+      dfs(i, 0);
+    }
+    REP(i, C.N) {
+      ans.at(i) = dfs(i, 0);
+    }
+  }
+
+  void output()
+  {
+    FORA(a, ans) pr(a);
+  }
+} s;
+
+int main()
+{
+  s.solve();
+  s.output();
+  return 0;
+}
